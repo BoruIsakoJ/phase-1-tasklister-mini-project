89c689ac9bc1307da7193837fae9afa3c9798c41
Boru Isako <isco.jaldy@gmail.com>
2025-03-13 12:16:59 +0300

    Complete labs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const formEl = document.querySelector('form')
+  formEl.addEventListener('submit', function (event) {
+    event.preventDefault()
+    createToDo(event.target["new-task-description"].value)
+  })
 });
+function createToDo(todo) {
+  const li = document.createElement('li')
+  const liBtn = document.createElement('button')
+
+  liBtn.textContent = 'Done'
+  liBtn.className = 'liBtn'
+  liBtn.addEventListener('click', hanldeDelete)
+
+  li.textContent =`${todo} `
+  li.appendChild(liBtn)
+  document.querySelector('#tasks').appendChild(li)
+  const inputEl =document.getElementById('new-task-description')
+  inputEl.value =""
+}
+function hanldeDelete(event){
+  event.target.parentElement.remove()
+}
